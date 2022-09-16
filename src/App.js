e058b621ee18d613dbe112ec246b7aca1be97f66
Jitendra Negi <jitendranegi576@gmail.com>
2022-09-16 20:55:24 +0530

    setup react ecommerce project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecommerce Project</h1>
+    </div>
+  );
+}
+
+export default App;
